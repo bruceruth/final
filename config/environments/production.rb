c212a712452d451ccf92879c8dc94c1b18001e22
ncistudent1 <ruth.stephenson1@gmail.com>
2016-07-08 20:41:03 +0000

    took out env static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  #config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -81,6 +81,6 @@ Rails.application.configure do
   
   #config.action_mailer.delivery_method = :smtp
 # SMTP settings for gmail
-#config.action_mailer.smtp_settings = {
+config.action_mailer.smtp_settings = {
 
 end
