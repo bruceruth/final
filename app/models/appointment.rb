5c52bc743f3363645506b8fed987f1e37bfcd082
ncistudent1 <ruth.stephenson1@gmail.com>
2016-07-21 21:54:54 +0000

    added email validation to appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -9,12 +9,22 @@
 ##
 
 class Appointment < ActiveRecord::Base
+##
+## @reference http://api.rubyonrails.org/classes/ActiveModel/Validations/ClassMethods.html#method-i-validate
+## @author Ruth Stephenson, X1500935
+##
     validates :timeslot, uniqueness: {scope: :date} #this will return a validation error if an oppointment exists with the same date and timeslot
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+    
     belongs_to :user
     has_many :options
-    
+##
+## @reference Taken from Tutorial "Implementing a Search Functionality"
+## @author Adriana Chis
+## @author Ruth Stephenson - adapted for my own particular search.
+##   
     def self.search(search_for)
-        #Appointment.where('name = ?', search_for)
+        #Appointment.where('name Like ?', search_for)
         Appointment.where('date = ?', search_for)
     end
 
