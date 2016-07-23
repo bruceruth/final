501ab31c8ac7cbebbd8f7236d3fe9b05f0c756f5
ncistudent1 <ruth.stephenson1@gmail.com>
2016-07-23 15:14:19 +0000

    enabled both sendgrid and gmail for password reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,15 +109,15 @@ config.assets.compile = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
-  #config.action_mailer.smtp_settings = {
-  #address: "smtp.gmail.com",
-  #port: 587,
-  #domain: ENV["GMAIL_DOMAIN"],
-  #authentication: "plain",
-  #enable_starttls_auto: true,
-  #user_name: ENV["angeltarotcarrick@gmail.com"],
-  #password: ENV["angeltarot"]
-  #}
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: ENV["GMAIL_DOMAIN"],
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["angeltarotcarrick@gmail.com"],
+  password: ENV["angeltarot"]
+  }
  
 
   ActionMailer::Base.smtp_settings = {
