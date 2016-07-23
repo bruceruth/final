3a22d2d3fde443358de0ce7336bc1c18e005b32b
ncistudent1 <ruth.stephenson1@gmail.com>
2016-07-23 14:37:46 +0000

    changed css style

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,16 @@ Rails.application.configure do
 ## @author Ruth Stephenson  x15009335 (unused)
 ##
 
+  config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: ENV['GMAIL_DOMAIN'],
+  authentication: 'plain',
+  enable_starttls_auto: true,
+  user_name: ENV['GMAIL_USERNAME'],
+  password: ENV['GMAIL_PASSWORD']
+  }
+
   #set up email server!
   #ActionMailer::Base.delivery_method = :smtp
   #ActionMailer::Base.smtp_settings = {
